d8c310e30d4d8ffce92e24eddce02089e6e6414b
SamB032 <70594311+SamB032@users.noreply.github.com>
2023-05-31 16:03:41 +0100

    adds easy bot to the board

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,11 @@
 // import {Link, useLocation} from 'react-router-dom';
 import '../styles/Navbar.css';
-import Button from 'react-bootstrap/Button';
 
 function Navbar() {
     return (
         <div className="navbar">
             <div className="links">
-                <Button bsPrefix="nav-button" className="nav-button">Login</Button>{' '}
+                <button bsPrefix="nav-button" className="nav-button">Login</button>{' '}
             </div>
         </div>
     );
