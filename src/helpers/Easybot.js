737ddd5dc6ecccd2d35c16284bdbe43b829824ae
SamB032 <70594311+SamB032@users.noreply.github.com>
2023-06-01 14:55:04 +0100

    Adds minimax algorithm to med and hard difficulties

--- src/helpers/Easybot.js
+++ src/helpers/Easybot.js
@@ -1,6 +1,8 @@
-//The easy bot just picks a column at random to drop a disk
-export function easyBotPredict(validCoordinates) {
-    console.log(validCoordinates);
+import {getValidCoordinates} from "./findAvaliableColumns";
+
+//The easy bot just picks a column at random from avaliable columns
+export function easyBotPredict(board, depth) {
+    const validCoordinates = getValidCoordinates(board);
     const randomIndex = Math.floor(Math.random() * validCoordinates.length); //Makes a random prediciton from 0 to length avaliable columns
     return validCoordinates[randomIndex][1]; //Extracts and returns the column value
 }
